8f113e364648f6e3dd37c82dcc58ccdd16219e91
Shreeya Pandey <131512383+therealsheero@users.noreply.github.com>
2026-05-09 23:10:16 +0530

    changing bulk download

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -8,15 +8,10 @@ router.post("/login", adminController.adminLogin);
 router.get("/calendar", adminAuth, adminController.getCalendarOverview);
 router.get("/tokens", adminAuth, adminController.getTokensByDate);
 router.get("/dashboard", adminAuth, adminController.getDateDashboard);
-router.get(
-  "/physical-reference-entry",
-  adminAuth,
-  adminController.getTodayPhysicalReferenceEntries
-);
+router.get("/physical-reference-entry",adminAuth, adminController.getTodayPhysicalReferenceEntries);
 router.put("/update-slots", adminAuth, adminController.updateSlots);
 router.post("/set-tokens-per-hour", adminAuth, adminController.updateTokensPerHour);
 router.get("/tokens-per-hour", adminController.getTokensPerHourPublic);
-// ✅ EXPORT CSV
 //router.get("/export-csv", adminAuth, adminController.exportTodayCSV);
 router.get("/priority-settings", adminController.getPrioritySettings);
 
