f0b4611b9f94c83907ce90095a09bee6133f4e06
Shreeya Pandey <131512383+therealsheero@users.noreply.github.com>
2026-05-09 23:13:54 +0530

    changing token type logic

--- backend/src/services/token.service.js
+++ backend/src/services/token.service.js
@@ -8,52 +8,7 @@ function getPriorityFlag(age, gender, divyang) {
   if (gender === "Female" && age >= 50) return "P";
   return "N";
 }
-//async function generateDailyToken(name, mobile, date, age, gender, divyang, mode) {
-//  const priority = getPriorityFlag(age, gender, divyang);
-//  const key = `${mode}${priority}`; 
-//  await new Promise((resolve, reject) => {
-//    db.run(
-//      `INSERT OR IGNORE INTO daily_token_counters (date) VALUES (?)`,
-//      [date],
-//      err => (err ? reject(err) : resolve())
-//    );
-//  });
-//
-//  const columnMap = {
-//    AP: "ap_count",
-//    AN: "an_count",
-//    WP: "wp_count",
-//    WN: "wn_count"
-//  };
-//
-//  const column = columnMap[key];
-//
-//  const count = await new Promise((resolve, reject) => {
-//    db.run(
-//      `
-//      UPDATE daily_token_counters
-//      SET ${column} = ${column} + 1
-//      WHERE date = ?
-//      `,
-//      [date],
-//      err => {
-//        if (err) return reject(err);
-//
-//        db.get(
-//          `SELECT ${column} FROM daily_token_counters WHERE date = ?`,
-//          [date],
-//          (err, row) => {
-//            if (err) reject(err);
-//            else resolve(row[column]);
-//          }
-//        );
-//      }
-//    );
-//  });
-//
-//  const seq = pad(count);
-//  return {token: `${name}-${mobile}-${seq}-${key}`, priority};
-//}
+
 async function generateDailyToken(
   name,
   mobile,
@@ -75,22 +30,6 @@ async function generateDailyToken(
     );
   });
 
-  // ?? GLOBAL COUNT
-//  const seq = await new Promise((resolve, reject) => {
-//    db.run(
-//      `UPDATE daily_token_counters
-//       SET last_token = last_token + 1
-//       WHERE date = ?`,
-//      [date],
-//      err => {
-//        if (err) return reject(err);
-//        db.get(
-//          `SELECT last_token FROM daily_token_counters WHERE date = ?`,
-//          [date],
-//          (err, row) => err ? reject(err) : resolve(row.last_token)
-//        );
-//      }
-//    );
   const seq = await new Promise((resolve, reject) => {
   db.get(
     `
@@ -114,7 +53,7 @@ async function generateDailyToken(
     WP: "wp_count",
     WN: "wn_count",
     WL: "wl_count",
-    AL: "al_count" // WL counts as WN
+    AL: "al_count" 
   };
 
   const col = map[tokenType];
